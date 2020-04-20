925d6384827dd77b2e6e1cf9c6cfea610dedda5d
Jamal Rjoul <jamensky1991@gmail.com>
2020-04-20 08:21:23 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,18 +1,11 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  def initialize( type, db)
-    @id = id
-    @name = name
-    @type = type
-    @db = db
-  end
-=begin
-  def save(name, type, db)
-    sql = 'INSERT INTO pokemon(name, type) VALUES(?, ?)'
-    @db.execute(sql, name, type, db)
-    #@id =
-  end
-=end
-
+  def initialize(id, name, type, db)
+    @id = id 
+    @name = name 
+    @type = type 
+    @db = db 
+  end 
+  
 end
